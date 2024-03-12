74c901bb45c400840939c69169e0e3886918687c
ashish <ashish50514561@gmail.com>
2024-03-12 13:40:45 +0530

    reverted Gradio

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def generate_response(payload):
     aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
     region = os.getenv("REGION")
-    endpoint_name = os.getenv("ENDPOINT")
+    endpoint_name = os.getenv("ENDPOINT_NAME")
 
     client = boto3.client(
         "runtime.sagemaker",
